8f0a29c9ba6132711232a7457561d2aa52b68143
Jon Gill <jonpgill1@gmail.com>
2022-03-18 21:35:49 +0000

    wire up Netlify form submission

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,12 +1,38 @@
 import React from 'react';
 
 export default function Contact() {
+  const [name, setName] = React.useState('');
+  const [email, setEmail] = React.useState('');
+  const [message, setMessage] = React.useState('');
+
+  function encode(data) {
+    return Object.keys(data)
+      .map(
+        (key) => encodeURIComponent(key) + '=' + encodeURIComponent(data[key])
+      )
+      .join('&');
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    fetch('/', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+      body: encode({ 'form-name': 'contact', name, email, message }),
+    })
+      .then(() => {
+        alert('Message sent!');
+      })
+      .catch((error) => alert(error));
+  }
+
   return (
     <section id='contact' className='relative'>
       <div className='container px-5 py-10 mx-auto flex sm:flex-nowrap flex-wrap'>
         <form
           netlify
           name='contact'
+          onSubmit={handleSubmit}
           className='lg:w-1/3 md:w-2/3 flex flex-col mx-auto w-full md:py-8 mt-8 md:mt-0'
         >
           <h2 className='text-white sm:text-4xl text-3xl mb-2 font-medium title-font'>
@@ -25,6 +51,7 @@ export default function Contact() {
               id='name'
               name='name'
               className='w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out'
+              onChange={(e) => setName(e.target.value)}
             />
           </div>
           <div className='relative mb-4'>
@@ -36,6 +63,7 @@ export default function Contact() {
               id='email'
               name='email'
               className='w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out'
+              onChange={(e) => setEmail(e.target.value)}
             />
           </div>
           <div className='relative mb-4'>
@@ -49,6 +77,7 @@ export default function Contact() {
               id='message'
               name='message'
               className='w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 h-32 text-base outline-none text-gray-100 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out'
+              onChange={(e) => setMessage(e.target.value)}
             />
           </div>
           <button
