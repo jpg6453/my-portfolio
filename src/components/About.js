7b7e0f1d3b4a202af14b5a47a4fb894855f50e64
Jon Gill <jonpgill1@gmail.com>
2022-03-19 16:07:34 +0000

    fix margin for md screens

--- src/components/About.js
+++ src/components/About.js
@@ -3,13 +3,13 @@ import React from 'react';
 export default function About() {
   return (
     <section id='about'>
-      <div className='container px-4 flex py-12 md:p-6 lg:px-24 lg:py-10 xl:px-28 xl:py-20 lg:flex-row flex-col items-center md:mt-20'>
+      <div className='container md:mx-auto px-4 flex py-12 md:p-6 lg:px-24 lg:py-10 xl:px-40 xl:py-20 lg:flex-row flex-col items-center md:mt-20'>
         <div className='lg:flex-grow md:w-full lg:pr-24 flex flex-col md:items-center md:text-center lg:items-start lg:text-left mb-16 md:mb-8 items-start text-left'>
           <h1 className='title-font font-extrabold tracking-tight md:text-6xl text-4xl xl:text-6xl mb-4 text-white'>
             <span className='block md:mb-1 lg:mb-0'>Jon Gill</span>
             <span className='block text-green-400'>Front End Developer</span>
           </h1>
-          <p className='mb-8 text-xl font-semibold md:px-10 lg:px-0'>
+          <p className='mb-8 text-xl font-semibold md:px-5 lg:px-0'>
             Former Senior Retail Buyer. Relocated to Ireland from Sheffield, UK.
             Passionate about tech and the increasing role it plays in our lives.
           </p>
